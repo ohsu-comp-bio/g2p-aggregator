8d7159e9a16195b579a45e64eb9dca1b22b25019
EC2 Default User <ec2-user@ip-172-31-7-88.us-west-2.compute.internal>
2017-05-30 23:39:35 +0000

    check env variables

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-echo install & configure app
+#!/bin/bash
+echo install and configure app
 
-
-
-if [ `alias | grep dc | wc -l` != 0 ]; then
+#
+echo checking docker compose alias
+if [ `alias | grep dc | wc -l` != 1 ]; then
 
 echo Please configure environmental variables
 echo
@@ -23,9 +24,9 @@ exit 1
 fi
 
 
-
-# for github access
-if [ ! -f ~/g2p-aggregator ]; then
+#
+echo checking clone repo
+if [ ! -d ~/g2p-aggregator ]; then
   # clone repo
   cd ~
   git clone https://github.com/ohsu-comp-bio/g2p-aggregator.git
@@ -35,19 +36,18 @@ ELASTIC_PORT=9200
 KIBANA_PORT=5601
 EOT
 
-  dc up -d
 fi
 
 
-if [ ! -f ~/g2p-aggregator/util/elastic/backups ]; then
+echo checking backup dir
+if [ ! -d ~/g2p-aggregator/util/elastic/backups ]; then
   echo copy snapshot back up to this host... e.g.
   echo scp -r -i [your key] ~/g2p-aggregator/util/elastic/backups/ [user]@[host]:g2p-aggregator/util/elastic/backups
   mkdir ~/g2p-aggregator/util/elastic/backups
 fi
 
 
-if [ -f ~/g2p-aggregator/util/elastic/backups ]; then
+if [ -d ~/g2p-aggregator/util/elastic/backups ]; then
   cd ~/g2p-aggregator
-  alias dc=docker-compose
   dc up -d
 fi
