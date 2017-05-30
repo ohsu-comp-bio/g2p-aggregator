5e93450538c13dcff08a2c411efdeb202932fd2a
EC2 Default User <ec2-user@ip-172-31-7-88.us-west-2.compute.internal>
2017-05-30 23:37:49 +0000

    add index close/open around restore

--- util/elastic/snapshot-restore.sh
+++ util/elastic/snapshot-restore.sh
@@ -8,5 +8,6 @@ if [ -z "$1" ]
     ES=$1
 fi
 
-
-curl  -XPOST $ES'/_snapshot/backups/'$2'/restore
+curl -XPOST  $ES/_all/_close
+curl  -XPOST $ES'/_snapshot/backups/'$2'/_restore'
+curl -XPOST  $ES/_all/_open
