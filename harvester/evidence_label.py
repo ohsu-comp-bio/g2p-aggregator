53fa6801760d833e4f488aedadd974c20e2d20fa
Brian Walsh <brian@bwalsh.com>
2017-07-07 16:07:27 -0700

    AMP/ASCO/CAP Guidelines

--- harvester/evidence_label.py
+++ harvester/evidence_label.py
@@ -3,28 +3,38 @@
 # 'FDA guidelines', 'preclinical', 'trials', 'NCCN guidelines',  or
 # 'European LeukemiaNet Guidelines'
 
+# see https://docs.google.com/spreadsheets/d/1j9AKdv1k87iO8qH-ujnW3x4VusbGoXDjdb5apUqJsSI/edit#gid=1415903760
+
 
 def evidence_label(evidence, association, na=False):
-    fda = ['fda guidelines', 'fda-approved', 'fda approved', '1']
-    nccn = ['nccn guidelines', 'nccn-approved', 'nccn approved']
+    fda = ['fda guidelines', 'fda-approved', 'fda approved', '1', 'guideline']
+    nccn = ['nccn guidelines', 'nccn-approved', 'nccn approved',
+            'nccn/cap guidelines']
     eln = ['european leukemianet guidelines']
-    preclinical = ['preclinical', 'pre-clinical', '2', '3']
-    trials = ['early trials', 'late trials', 'phase 2',
-              'phase ii', 'phase 1', 'phase i']
+    preclinical = ['preclinical', 'pre-clinical', '3']
+    trials_c = ['early trials', 'late trials', 'phase 2', '2',
+                'phase ii', 'phase 1', 'phase i', 'clinical trial',
+                'clinical study']
+    trials_b = ['phase 3', 'phase iii']
+    civic_a = ['a']
+    civic_b = ['b']
+    civic_c = ['c']
+    civic_d = ['d', 'e']
+
     cr = ['case report']
+    oncokb_b = ['compelling clinical evidence supports the biomarker as being predictive of response to a drug in this indication but neither biomarker and drug are standard of care']  # NOQA
+    oncokb_c = ['compelling biological evidence supports the biomarker as being predictive of response to a drug but neither biomarker and drug are standard of care']  # NOQA
 
     ev_lab = {
-        'FDA guidelines': fda,
-        'NCCN guidelines': nccn,
-        'European Leukemia Net guidelines': eln,
-        'preclinical': preclinical,
-        'in trials': trials,
-        'case report': cr
+        'A': fda + nccn + eln + civic_a,
+        'B': trials_b + oncokb_b + civic_b,
+        'C': trials_c + oncokb_c + civic_c,
+        'D': cr + preclinical + civic_d
     }
 
     for item in ev_lab:
         for opt in ev_lab[item]:
-            if opt in evidence.lower():
+            if evidence and opt in evidence.lower():
                 association['evidence_label'] = item
     if 'evidence_label' not in association:
         if na:
