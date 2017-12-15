2c891945d7d8445734e0e5fc7e5b567473b284df
Brian Walsh <brian@bwalsh.com>
2017-12-15 10:44:35 -0800

    pmkb api down; use their file

--- harvester/pmkb.py
+++ harvester/pmkb.py
@@ -4,16 +4,114 @@ import sys
 import requests
 from inflection import parameterize, underscore
 import json
+import evidence_label as el
+import evidence_direction as ed
+import mutation_type as mut
 
 
 def harvest(genes=None):
-    with open("pmkb.json", "r") as ins:
+    with open("../data/pmkb_interpretations.json", "r") as ins:
         for line in ins:
-            yield json.loads(line)
+            interpretations = json.loads(line)['interpretations']
+            for interpretation in interpretations:
+                yield interpretation
 
 
 def convert(interpretation):
-    yield interpretation
+    """create feature_association from pmkb evidence"""
+    for variant in interpretation['variants']:
+        if 'coordinates' in variant:
+            # '7:140453135-140453136'
+            # '3:41266097-41266099, 3:41266100-41266102, 3:41266103-41266105, 3:41266106-41266108, 3:41266109-41266111, 3:41266112-41266114, 3:41266124-41266126, 3:41266136-41266138'  # NOQA
+            s = variant['coordinates']
+            if not s:
+                continue
+            coordinates = s.replace(' ', '').split(',')
+            for coordinate in coordinates:
+                feature = {}
+                feature['geneSymbol'] = variant['gene']['name']
+                feature['name'] = variant['name']
+                a = coordinate.split(':')
+                chromosome = a[0]
+                start, stop = a[1].split('-')
+                feature['start'] = int(start)
+                feature['end'] = int(stop)
+                feature['chromosome'] = str(chromosome)
+                feature['referenceName'] = 'GRCh37/hg19'
+                feature['biomarker_type'] = mut.norm_biomarker(
+                                                variant['variant_type']
+                                            )
+
+                attributes = {}
+                for key in variant.keys():
+                    if key not in ['coordinates', 'name', 'gene']:
+                        attributes[key] = {'string_value': variant[key]}
+                feature['attributes'] = attributes
+
+                # TODO - replace w/ biocommons/hgvs ?
+                if 'dna_change' in variant:
+                    dna_change = variant['dna_change']
+                    if dna_change and '>' in dna_change:
+                        prefix, alt = dna_change.split('>')
+                        ref = prefix[-len(alt):]
+                        if len(ref) > 0 and len(alt) > 0:
+                            feature['ref'] = ref
+                            feature['alt'] = alt
+
+                gene = variant['gene']['name']
+
+                association = {}
+
+                if attributes['amino_acid_change']['string_value']:
+                    association['variant_name'] = attributes['amino_acid_change']['string_value']
+
+                # association['evidence_label'] = interpretation['tier']
+                association['source_link'] = 'https://pmkb.weill.cornell.edu/variants/{}'.format(variant['id'])
+                association = el.evidence_label(str(interpretation['tier']),
+                                                association, na=True)
+                association = ed.evidence_direction(
+                                str(interpretation['tier']),
+                                association, na=True)
+
+                association['description'] = interpretation['interpretation']
+                # TODO pmkb does not break out drug !?!?
+                # association['environmentalContexts'] = []
+
+                for tumor in interpretation['tumors']:
+                    association['phenotype'] = {
+                        'description': tumor['name']
+                    }
+                    association['drug_labels'] = 'NA'
+                    association['evidence'] = [{
+                        "evidenceType": {
+                            "sourceName": "pmkb"
+                        },
+                        'description': str(interpretation['tier']),
+                        'info': {
+                            'publications': [
+                                'http://www.ncbi.nlm.nih.gov/pubmed/{}'.format(c['pmid']) for c in interpretation['citations']  # NOQA
+                            ]
+                        }
+                    }]
+                    # add summary fields for Display
+                    if len(interpretation['citations']) > 0:
+                        association['publication_url'] = 'http://www.ncbi.nlm.nih.gov/pubmed/{}'.format(interpretation['citations'][0]['pmid'])  # NOQA
+                    feature_association = {'genes': [gene],
+                                           'features': [feature],
+                                           'feature_names':
+                                           '{} {}'.format(
+                                                feature["geneSymbol"],
+                                                feature["name"]
+                                            ),
+                                           'association': association,
+                                           'source': 'pmkb',
+                                           'pmkb': {
+                                            'variant': variant,
+                                            'tumor': tumor,
+                                            'tissues':
+                                            interpretation['tissues']
+                                           }}
+                    yield feature_association
 
 
 def harvest_and_convert(genes):
