109333ae0c687b693eafc2b75487ff001d077ef9
Brian Walsh <brian@bwalsh.com>
2018-04-02 16:41:03 -0700

    #99

--- harvester/feature_enricher.py
+++ harvester/feature_enricher.py
@@ -152,7 +152,7 @@ def enrich(feature, feature_association):
 
 
         enriched_features = []
-        if len(description_parts[0].split('-')) == 2:
+        if len(description_parts[0].split('-')) == 2 and len(description_parts[0].split('-')[1]) > 1:
             fusion_donor, fusion_acceptor = description_parts[0].split('-')
             feature_fusion_donor = _enrich_gene(copy.deepcopy(feature), fusion_donor, provenance_rule='is_fusion_donor')  # NOQA
             feature_fusion_donor['geneSymbol'] = fusion_donor
