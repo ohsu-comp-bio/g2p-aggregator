3e6050db560afa07dcda7432492c635b5b37dfb7
Brian Walsh <brian@bwalsh.com>
2018-04-05 13:14:14 -0700

    additional gene attributes

--- harvester/gene_enricher.py
+++ harvester/gene_enricher.py
@@ -5,14 +5,32 @@ import json
 # load gene names
 # ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/json/non_alt_loci_set.json
 GENES = {}
+ALIASES = {}
 
 # trim payload, we only need symbol and ensembl
 data = json.load(open('../data/non_alt_loci_set.json'))
 for doc in data['response']['docs']:
-    GENES[doc['symbol']] = {'ensembl_gene_id': doc.get('ensembl_gene_id', None)}
+    gene = {
+        'symbol': doc['symbol'],
+        'ensembl_gene_id': doc.get('ensembl_gene_id', None),
+        'entrez_id': doc.get('entrez_id', None)
+        }
+    GENES[doc['symbol']] = gene
+    if gene['ensembl_gene_id']:
+        ALIASES[gene['ensembl_gene_id']] = gene
+    if gene['entrez_id']:
+        ALIASES[gene['entrez_id']] = gene
+    for alias in doc.get('alias_symbol', []):
+        ALIASES[alias] = gene
+    for prev in doc.get('prev_symbol', []):
+        ALIASES[prev] = gene
 data = None
 
 
-def get_gene(symbol):
-    """ return gene for symbol """
-    return GENES.get(symbol, None)
+def get_gene(identifier):
+    """ return gene for identifier """
+    for store in [GENES, ALIASES]:
+        gene = store.get(identifier, None)
+        if gene:
+            return gene
+    return None
