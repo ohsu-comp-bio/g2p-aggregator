7e7e2f2b60a1c1ebfdebee4bcf242a3e50f1e571
Brian Walsh <brian@bwalsh.com>
2018-06-14 17:39:34 -0700

    wip

--- smmartnotebook/smmartnotebook/query.py
+++ smmartnotebook/smmartnotebook/query.py
@@ -1,4 +1,3 @@
-
 # so we can call the g2p service
 import requests
 import json
@@ -8,6 +7,7 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 import datetime
+import traceback
 
 
 def to_df(generator, feature):
@@ -18,85 +18,103 @@ def to_df(generator, feature):
             drugs = [ec.get('term', ec['description'])
                      .encode('utf-8')
                      for ec in hit['association']['environmentalContexts']]
-            description = hit['association']['description'].encode('utf-8')
-            source = hit['source'].encode('utf-8')
+            drugs = b",".join(drugs).decode("utf-8")
+            description = hit['association']['description'] # .decode('utf-8')
+            source = hit['source'] # .decode('utf-8')
             evidence_label = hit['association']['evidence_label']
             genes = hit['genes']
+            genes = ",".join(genes)
             id = hit['evidence.id']
-            phenotype = hit['association']['phenotype']['type']['term']\
-                .encode('utf-8')
+            phenotypes = [phenotype['term'].encode('utf-8') for phenotype in hit['association']['phenotypes']]
+            phenotypes = b",".join(phenotypes).decode("utf-8")
             publications = []
             matches = []
 
             try:
-                # compare the queried feature vs all the features in the hit.
-                # highlight the matches
-                f = feature
-                if f:
-                    for f2 in hit.get('features', []):
-                        if not f2:
-                            continue
-                        for s in f.get('synonyms', []):
-                            if s in f2.get('synonyms', []):
-                                matches.append(s)
-                        for p in f.get('pathways', []):
-                            if p in f2.get('pathways', []):
-                                matches.append('pathway')
-                        for sp in f.get('swissprots', []):
-                            if sp in f2.get('swissprots', []):
-                                matches.append(sp)
+#                 # compare the queried feature vs all the features in the hit.
+#                 # highlight the matches
+#                 f = feature
+#                 if f:
+#                     for f2 in hit.get('features', []):
+#                         if not f2:
+#                             continue
+#                         for s in f.get('synonyms', []):
+#                             if s in f2.get('synonyms', []):
+#                                 matches.append(s)
+#                         for p in f.get('pathways', []):
+#                             if p in f2.get('pathways', []):
+#                                 matches.append('pathway')
+#                         for sp in f.get('swissprots', []):
+#                             if sp in f2.get('swissprots', []):
+#                                 matches.append(sp)
 
-                        for pe in f.get('protein_effects', []):
-                            pe = pe.split(':')[1]
-                            for pe2 in f2.get('protein_effects', []):
-                                pe2 = pe2.split(':')[1]
-                                if pe == pe2:
-                                    matches.append(pe)
+#                         for pe in f.get('protein_effects', []):
+#                             pe = pe.split(':')[1]
+#                             for pe2 in f2.get('protein_effects', []):
+#                                 pe2 = pe2.split(':')[1]
+#                                 if pe == pe2:
+#                                     matches.append(pe)
 
-                        for pd in f.get('protein_domains', []):
-                            for pd2 in f2.get('protein_domains', []):
-                                if pd['name'] == pd2['name']:
-                                    matches.append(pd['name'])
+#                         for pd in f.get('protein_domains', []):
+#                             for pd2 in f2.get('protein_domains', []):
+#                                 if pd['name'] == pd2['name']:
+#                                     matches.append(pd['name'])
 
-                        if f.get('sequence_ontology', {'name': 'X'}) == \
-                                f2.get('sequence_ontology', {'name': None}):
-                            matches.append(f['sequence_ontology']['name'])
+#                         if f.get('sequence_ontology', {'name': 'X'}) == \
+#                                 f2.get('sequence_ontology', {'name': None}):
+#                             matches.append(f['sequence_ontology']['name'])
 
-                        if f.get('geneSymbol', None) == f2.get('geneSymbol',
-                                                               None):
-                            matches.append(f.get('geneSymbol', None))
+#                         if f.get('geneSymbol', None) == f2.get('geneSymbol',
+#                                                                None):
+#                             matches.append(f.get('geneSymbol', None))
 
-                matches = list(set(matches))
+#                 matches = list(set(matches))
 
                 for e in hit['association']['evidence']:
                     for p in e['info']['publications']:
                         publications.append(p)
+                publications = ",".join(publications)
+
 
                 yield {'id': id,
                        'source': source,
                        'evidence_label': evidence_label,
                        'drugs': drugs,
                        'description': description,
-                       'phenotype': phenotype,
+                       'phenotypes': phenotypes,
                        'publications': publications,
-                       'matches': matches,
+#                        'matches': matches,
                        'genes': genes
                        }
             except Exception as e:
                 print('!!! {}'.format(e))
+                traceback.print_exc()
 
     try:
-        return pd.DataFrame.from_records(to_dict(generator, feature),
-                                         index='id')
-    except Exception as e:
+        return pd.DataFrame.from_records(to_dict(generator, feature), index='id')
+    except TypeError:
         # might be empty
         return pd.DataFrame()
+    except:
+        raise
+
 
 
 def query(url, features, verify=True, timeout=60*5):
     """ query the /api/v1/features/associations endpoint
         returns a tuple of (queries, data_frames, summary)
     """
+    query_weights = {
+        'alleles': 100,
+        '~location': 90,
+        '~range': 50,
+        'protein_effects': 40,
+        'protein_domains': 30,
+        '~biomarker_type': 20,
+        'genes': 10,
+        'pathways': 0
+    }
+
     queries = []
     data_frames = {}
     t = datetime.datetime.now()
@@ -116,7 +134,7 @@ def query(url, features, verify=True, timeout=60*5):
         data_frames[allele][name] = to_df(location_query['hits'],
                                           location_query['feature'])
         # now that we have the associations in a dataframe,
-        # lets recast the hits to a hit count
+        # lets recast the hits to a hit count * weight
         evidence_ids.extend([hit['evidence.id']
                             for hit in location_query['hits']])
         location_query['hits'] = len(location_query['hits'])
@@ -132,8 +150,9 @@ def query(url, features, verify=True, timeout=60*5):
     for q in queries:
         if q['allele'] not in queries_summary:
             queries_summary[q['allele']] = {
-                'allele': q['allele'], 'biomarker_type': q['biomarker_type']}
+                'allele': q['allele'], 'biomarker_type': q['biomarker_type'], 'weighted_hits': 0}
         queries_summary[q['allele']]['hits-{}'.format(q['name'])] = q['hits']
+        queries_summary[q['allele']]['weighted_hits'] = q['hits'] * query_weights.get(name, 0)
 
     summary = pd.DataFrame.from_records([queries_summary[k]
                                         for k in queries_summary.keys()])
