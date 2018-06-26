69771c9a6d5a876e52814f69d0b09cac5b176f06
Brian Walsh <brian@bwalsh.com>
2018-06-26 13:11:09 -0700

    test get_transcripts; fix test_ambiguous

--- harvester/tests/integration/test_gene_enricher.py
+++ harvester/tests/integration/test_gene_enricher.py
@@ -5,7 +5,7 @@ import gene_enricher  # NOQA
 
 
 def test_simple():
-    tp53 = gene_enricher.get_genes('TP53')
+    tp53 = gene_enricher.get_gene('TP53')
     assert(tp53)
     assert tp53 == [{'symbol': u'TP53', 'entrez_id': u'7157',
                     'ensembl_gene_id': u'ENSG00000141510'}]
@@ -14,7 +14,6 @@ def test_simple():
 def test_simple_normalize_feature_association():
     feature_association = {'genes': ['TP53']}
     gene_enricher.normalize_feature_association(feature_association)
-    print feature_association['gene_identifiers']
     assert 'gene_identifiers' in feature_association
     assert feature_association['gene_identifiers'] == [
         {'symbol': u'TP53', 'entrez_id': u'7157',
@@ -24,8 +23,18 @@ def test_simple_normalize_feature_association():
 
 def test_ambiguous():
     """ "ABC1" can point to both "ABCA1" and "HEATR6", """
-    genes = gene_enricher.get_genes('ABC1')
-    assert genes == [
-        {'symbol': u'ABCA1', 'entrez_id': u'19', 'ensembl_gene_id': u'ENSG00000165029'},
-        {'symbol': u'HEATR6', 'entrez_id': u'63897', 'ensembl_gene_id': u'ENSG00000068097'}
-    ]
+    try:
+        genes = gene_enricher.get_gene('ABC1')
+        assert False, 'should have raised error'
+    except ValueError as e:
+        pass
+
+
+def test_get_transcripts():
+    transcripts = gene_enricher.get_transcripts('BRAF')
+    assert len(transcripts) == 5
+
+
+def test_get_transcripts_nohits():
+    transcripts = gene_enricher.get_transcripts('FUBAR')
+    assert len(transcripts) == 0, 'no hits should have empty array'
