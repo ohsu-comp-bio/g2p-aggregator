aab3616432b14522c685330e525e94bc9b7a5aa0
Alex H. Wagner, PhD <awagner24@wustl.edu>
2019-06-24 15:13:01 -0500

    config path support

--- util/harvest-file-all.sh
+++ util/harvest-file-all.sh
@@ -1,9 +1,10 @@
-cd ~/g2p-aggregator/harvester;
+HARVEST_DIR=$G2P/harvester;
+cd $HARVEST_DIR;
 echo 'Launching harvesters'
 harvesters=( cgi_biomarkers jax civic oncokb pmkb molecularmatch brca )
 for h in "${harvesters[@]}"
 do
 	python harvester.py --harvesters $h --phase harvest --silos file >$h.harvest.log  &
-	echo -n $h'\n'
+	echo -n $h" "
 	sleep 1
 done
